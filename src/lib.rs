dd94c9014a250ec001db0dd3fb8f42392b172fbf
Maccraft123 <maccraft123mc@gmail.com>
2023-03-25 10:18:08 +0100

    Error handling

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,36 @@ pub extern crate libdogd;
 pub extern crate paste;
 pub extern crate bincode;
 pub extern crate serde;
+pub extern crate thiserror;
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, thiserror::Error)]
+pub enum DemoniteErr {
+    #[error("Bincode error: {0}")]
+    Serialize(String),
+    #[error("Input/Output error: {0}")]
+    Io(String),
+    #[error("Environment Variable Error: {0}")]
+    EnvVar(String),
+}
+
+// Eh... those errors don't have Serialize implemented.
+impl From<bincode::Error> for DemoniteErr {
+    fn from(other: bincode::Error) -> Self {
+        Self::Serialize(other.to_string())
+    }
+}
+
+impl From<std::io::Error> for DemoniteErr {
+    fn from(other: std::io::Error) -> Self {
+        Self::Io(other.to_string())
+    }
+}
+
+impl From<std::env::VarError> for DemoniteErr {
+    fn from(other: std::env::VarError) -> Self {
+        Self::EnvVar(other.to_string())
+    }
+}
 
 #[macro_export]
 macro_rules! decl_service {
@@ -9,25 +39,24 @@ macro_rules! decl_service {
         ::demonite::paste::paste! {
             #[derive(::demonite::serde::Serialize, ::demonite::serde::Deserialize, Debug)]
             #[serde(crate = "::demonite::serde")]
-            #[allow(non_camel_case_types)]
             pub enum $name {
                 $( [<_ $fn>]($($ty),*)),*
             }
+
             impl $name {
-                pub(crate) fn run(self) -> Vec<u8> {
+                pub(crate) fn run(self) -> Result<Vec<u8>, demonite::DemoniteErr> {
                     match self {
                         $($name::[<_ $fn>]($($arg),*) => 
-                          ::demonite::bincode::serialize(& $fn( $($arg),* ) ).unwrap()),*
+                          Ok(::demonite::bincode::serialize(& $fn( $($arg),* ) )?)),*
                     }
                 }
-                pub fn path() -> std::path::PathBuf {
-                    let mut path = std::path::PathBuf::from(std::env::var("XDG_RUNTIME_DIR")
-                        .expect("Failed to get XDG_RUNTIME_DIR"));
+                pub fn path() -> Result<std::path::PathBuf, demonite::DemoniteErr> {
+                    let mut path = std::path::PathBuf::from(std::env::var("XDG_RUNTIME_DIR")?);
                     path.push("demonite");
                     path.push(stringify!($name));
-                    path
+                    Ok(path)
                 }
-                pub(crate) fn launch() {
+                pub(crate) fn launch() -> Result<(), demonite::DemoniteErr> {
                     use std::{
                         env,
                         fs,
@@ -37,15 +66,13 @@ macro_rules! decl_service {
                     };
                     use ::demonite::bincode;
 
-                    if !Self::path().parent().unwrap().exists() {
-                        fs::create_dir(Self::path().parent().unwrap())
-                            .expect("Failed to ensure that XDG_RUNTIME_DIR/demonite/ dir exists");
+                    if !Self::path()?.parent().unwrap().exists() {
+                        fs::create_dir(Self::path()?.parent().unwrap())?;
                     }
-                    if Self::path().exists() {
-                        fs::remove_file(Self::path())
-                            .expect("Failed to ensure that socket file does not exist");
+                    if Self::path()?.exists() {
+                        fs::remove_file(Self::path()?)?;
                     }
-                    let sock = UnixListener::bind(Self::path()).expect("Failed to listen");
+                    let sock = UnixListener::bind(Self::path()?)?;
                     ::demonite::libdogd::log_info(format!("Listening on {:?}", sock.local_addr()));
                     for stream in sock.incoming() {
                         if let Ok(mut s) = stream {
@@ -57,7 +84,14 @@ macro_rules! decl_service {
                                 },
                             };
                             ::demonite::libdogd::log_debug(format!("Method call: {:?}", &variant));
-                            let response = variant.run();
+                            let response = match variant.run() {
+                                Ok(r) => r,
+                                Err(e) => {
+                                    ::demonite::libdogd::log_error(format!("Failed serializing response: {}", e));
+                                    bincode::serialize(&demonite::DemoniteErr::Serialize(e.to_string()))
+                                        .unwrap() // is it really going to never fail
+                                },
+                            };
                             match s.write_all(&response) {
                                 Ok(_) => (),
                                 Err(e) => {
@@ -66,16 +100,17 @@ macro_rules! decl_service {
                             }
                         }
                     }
+                    Ok(())
                 }
                 $(
-                    pub fn $fn($($arg: $ty),*) -> $ret {
+                    pub fn $fn($($arg: $ty),*) -> Result<$ret, demonite::DemoniteErr> {
                         use ::demonite::bincode;
                         use ::std::os::unix::net::UnixStream;
                         use ::std::io::Write;
-                        let packet: Vec<u8> = bincode::serialize(&Self::[<_ $fn>]($($arg),*)).unwrap();
-                        let mut stream = UnixStream::connect(Self::path()).unwrap();
-                        stream.write_all(&packet);
-                        bincode::deserialize_from(&stream).unwrap()
+                        let packet: Vec<u8> = bincode::serialize(&Self::[<_ $fn>]($($arg),*))?;
+                        let mut stream = UnixStream::connect(Self::path()?)?;
+                        stream.write_all(&packet)?;
+                        Ok(bincode::deserialize_from(&stream)?)
                     }
                  )*
             }
